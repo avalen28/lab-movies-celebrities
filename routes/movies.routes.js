62b5b33f2f9e677c8d65e76bb4306213aa170884
Alberto Valenzuela <avalen28@gmail.com>
2023-01-16 22:36:42 +0100

    adds movie cataloge and adds movie creator

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,14 +1,52 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const express = require("express");
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 // all your routes here
-router.get("/", (req, res, next) => res.render("movies/all-movies"));
+router.get("/", async (req, res, next) => {
+  try {
+    const moviesFromDB = await Movie.find({}).populate("cast");
+    console.log(moviesFromDB);
+    res.render("movies/all-movies", { moviesFromDB });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/create", async (req, res, next) => {
+  try {
+    const celebritiesFromDB = await Celebrity.find({});
+    res.render("movies/new-movie", { celebritiesFromDB });
+    console.log(celebritiesFromDB);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  try {
+    const { title, genre, plot, cast } = req.body;
+    if (typeof cast === "string") {
+      Movie.create({
+        title,
+        genre,
+        plot,
+        cast: [cast],
+      });
+    } else {
+      Movie.create({
+        title,
+        genre,
+        plot,
+        cast: cast,
+      });
+    }
+    res.redirect("/movies");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
-
-// try {
-//     await res.render("./movies/movies")
-// } catch (err) {
-//     next(err)
-// }
